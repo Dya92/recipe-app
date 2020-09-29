56a618ecaca07c2e0469f0363cc48cbe2cd8af2d
Diana <diana.barnea@gmail.com>
2020-09-29 19:10:07 +0300

    examples of jpa finding and implementing automatically findByRandomProperty

--- src/main/java/diana/springframework/controllers/IndexController.java
+++ src/main/java/diana/springframework/controllers/IndexController.java
@@ -1,14 +1,34 @@
 package diana.springframework.controllers;
 
+import diana.springframework.model.Category;
+import diana.springframework.model.UnitOfMeasure;
+import diana.springframework.repository.CategoryRepository;
+import diana.springframework.repository.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"","/","index"})
     public String getIndexPage() {
         System.out.println("some message...bla bla bla");
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat Id is: " + categoryOptional.get().getId());
+        System.out.println("UOM Id is: " + unitOfMeasureOptional.get().getId());
         return "index";
     }
 }
