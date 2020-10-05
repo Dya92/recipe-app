d093748d5d32312811846a63c0c0488c68b0fa7f
Diana <diana.barnea@gmail.com>
2020-10-05 21:37:20 +0300

    adding bootstrap webjar + a new view + tests

--- src/test/java/diana/springframework/service/RecipeServiceImplTest.java
+++ src/test/java/diana/springframework/service/RecipeServiceImplTest.java
@@ -8,9 +8,11 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 class RecipeServiceImplTest {
@@ -41,6 +43,22 @@ class RecipeServiceImplTest {
 
         //mokito gives an empty set by default
         //assertEquals(recipes.size(),0);
+
+    }
+
+    @Test
+    public void getRecipesByIdTest() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        Recipe recipeReturned = recipeService.findById(1L);
+
+        assertNotNull("Null recipe returned", recipeReturned);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
 
     }
 }
