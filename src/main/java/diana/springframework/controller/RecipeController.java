fb8b6e4222ed00f4250aa85a5b7494f104ed8bc6
Diana <diana.barnea@gmail.com>
2020-10-08 18:04:05 +0300

    deleting recipes

--- src/main/java/diana/springframework/controller/RecipeController.java
+++ src/main/java/diana/springframework/controller/RecipeController.java
@@ -2,10 +2,12 @@ package diana.springframework.controller;
 
 import diana.springframework.command.RecipeCommand;
 import diana.springframework.service.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @Controller
 public class RecipeController {
     private final RecipeService recipeService;
@@ -14,12 +16,14 @@ public class RecipeController {
         this.recipeService = recipeService;
     }
 
+    @GetMapping
     @RequestMapping("/recipe/{id}/show")
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe",recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
     }
 
+    @GetMapping
     @RequestMapping("recipe/new")
     public String newRecipe(Model model) {
         model.addAttribute("recipe", new RecipeCommand());
@@ -27,6 +31,7 @@ public class RecipeController {
         return "recipe/recipeForm";
     }
 
+    @GetMapping
     @RequestMapping("recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
@@ -41,5 +46,13 @@ public class RecipeController {
         RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
         // "redirect: ... " go to
         return "redirect:/recipe/" +  + savedCommand.getId() +"/show";
+    }
+
+    @GetMapping
+    @RequestMapping("recipe/{id}/delete")
+    public String deleteById(@PathVariable String id) {
+        log.debug("Deleting id: " + id);
+        recipeService.deleteById(Long.valueOf(id));
+        return "redirect:/";
     }
 }
